bc6c5907de7b104ab008e92b28d475f552ca262e
vijayaecaffeine <61559697+vijayaecaffeine@users.noreply.github.com>
2025-07-22 22:47:11 -0500

    installed tensorflow

---
+++ TensorFlow_P1.py
@@ -0,0 +1,10 @@
+#install tensorflow framework in Terminal of VSCode
+
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+
+# Start a TensorFlow session
+tf.print(hello)
+
+from tensorflow import keras
+
